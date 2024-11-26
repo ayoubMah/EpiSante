c929c68d3781b4296977d614533accf0dbeb68e9
ayoubMah <ayoubelmahjouby9@gmail.com>
2024-11-26 23:36:47 +0100

    make the spring security depandancy in command cuz we do not needed now, to remove the problem of sign in page

--- src/main/java/Episante/back/Controller/Controller.java
+++ src/main/java/Episante/back/Controller/Controller.java
@@ -1,4 +1,13 @@
 package Episante.back.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+    @GetMapping("/")
+    public String ayoub() {
+        return "Hello World";
+    }
+
 }
