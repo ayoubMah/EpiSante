4de8e5cc3a9cce5fa78e0ffed68ce4922bdf62a1
ayoubMah <ayoubelmahjouby9@gmail.com>
2024-11-28 01:42:58 +0100

    Modify the controller layer and make a clean code

--- src/main/java/Episante/back/Controller/PatientController.java
+++ src/main/java/Episante/back/Controller/PatientController.java
@@ -2,45 +2,78 @@ package Episante.back.Controller;
 
 import Episante.back.DAO.IPatientDao;
 import Episante.back.Entity.Patient;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
 @RestController
 @RequestMapping("/api/patients")
 public class PatientController {
+
+    // using loggers better than Sout , for debuging , errors , ....
+    private static final Logger logger = LoggerFactory.getLogger(PatientController.class);
+
 
     @Autowired
     private IPatientDao patientDao;
 
     @GetMapping
     public List<Patient> getAllPatients() {
+        logger.info("Get all patients");
         return patientDao.findAll();
     }
 
+
+    //using the ResponseEntity to see the status of the request
     @PostMapping
-    public Patient createPatient(@RequestBody Patient patient) {
-        return patientDao.save(patient);
+    public ResponseEntity<Patient> createPatient(@Valid @RequestBody Patient patient) {
+        Patient savedPatient = patientDao.save(patient);
+        logger.info("Patient created successfully: {}", savedPatient.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPatient);
     }
 
-    @PutMapping
-    public Patient updatePatient( @PathVariable Long id , @RequestBody Patient patientDetails) {
-        Patient patient = patientDao.findById(id).orElseThrow();
-
-        patient.setNom(patientDetails.getNom());
-        patient.setPrenom(patientDetails.getPrenom());
-        patient.setAge(patientDetails.getAge());
-        patient.setPoids(patientDetails.getPoids());
-        patient.setSexe(patientDetails.getSexe());
-
-        return patientDao.save(patient);
+// old version
+//    @PutMapping("/{id}")
+//    public Patient updatePatient( @PathVariable Long id , @RequestBody Patient patientDetails) {
+//        Patient patient = patientDao.findById(id).orElseThrow(() -> new RuntimeException("Patient not found with id: " + id)) ;
+//
+//        patient.setNom(patientDetails.getNom());
+//        patient.setPrenom(patientDetails.getPrenom());
+//        patient.setAge(patientDetails.getAge());
+//        patient.setPoids(patientDetails.getPoids());
+//        patient.setSexe(patientDetails.getSexe());
+//
+//        System.out.println("Patient updated successfully");
+//        return patientDao.save(patient);
+//
+//    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Patient> updatePatient(@PathVariable Long id , @Valid @RequestBody Patient patientDetails) {
+        Patient patient = patientDao.findById(id).orElseThrow(() -> new RuntimeException("Patient not found with id: " + id));
+        BeanUtils.copyProperties(patientDetails, patient); // with copyProperties method you just copy the propreties of your Patient to avoid setters
+        Patient updatedPatient = patientDao.save(patient);
+        logger.info("Patient updated successfully: {}", updatedPatient.getId());
+        return ResponseEntity.status(HttpStatus.OK).body(updatedPatient);
     }
 
-    @DeleteMapping
-    public void deletePatient(@PathVariable Long id) {
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Patient> deletePatient(@PathVariable Long id) {
+        if(!patientDao.existsById(id)) {
+            logger.error("Patient not found with id: " + id);
+        }
         patientDao.deleteById(id);
+        logger.info("Patient deleted successfully: {}", id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
-
 
 }
--- src/main/java/Episante/back/DataLoader.java
+++ src/main/java/Episante/back/DataLoader.java
@@ -18,38 +18,30 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         if (patientDao.count() == 0) { // Check if data already exists
 
+            Patient patient3 = new Patient();
+            patient3.setNom("Abou");
+            patient3.setNom("EL-Mahjouby");
+            patient3.setAge(21);
+            patient3.setPoids(57.9);
+            patient3.setSexe(Sexe.HOMME);
+
             Patient patient1 = new Patient();
-            patient1.setNom("Smith");
-            patient1.setPrenom("John");
-            patient1.setAge(30);
-            patient1.setPoids(75.5);
+            patient1.setNom("Abou");
+            patient1.setPrenom("Yassine");
+            patient1.setAge(21);
+            patient1.setPoids(68);
             patient1.setSexe(Sexe.HOMME);
 
             Patient patient2 = new Patient();
-            patient2.setNom("Nana");
-            patient2.setPrenom("Jane");
-            patient2.setAge(28);
-            patient2.setPoids(60.0);
-            patient2.setSexe(Sexe.FEMME);
-
-            Patient patient3 = new Patient();
-            patient1.setNom("EL-Mahjouby");
-            patient1.setPrenom("Ayoub");
-            patient1.setAge(21);
-            patient1.setPoids(57.9);
-            patient1.setSexe(Sexe.HOMME);
-
-            Patient patient4 = new Patient();
-            patient2.setNom("EL Hammi");
-            patient2.setPrenom("Ikram");
-            patient2.setAge(21);
-            patient2.setPoids(65.0);
+            patient2.setNom("Sharapova");
+            patient2.setPrenom("Maria");
+            patient2.setAge(37);
+            patient2.setPoids(78.6);
             patient2.setSexe(Sexe.FEMME);
 
             patientDao.save(patient1);
             patientDao.save(patient2);
             patientDao.save(patient3);
-            patientDao.save(patient4);
 
             System.out.println("Patient data loaded successfully");
         }else {
