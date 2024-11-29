e6752108eafd3475631d4a2b94bf3ef89fcceb7c
ayoubMah <ayoubelmahjouby9@gmail.com>
2024-11-29 20:53:42 +0100

    fix SpringConfiguration.java

--- src/main/java/Episante/back/SpringConfiguration.java
+++ src/main/java/Episante/back/SpringConfiguration.java
@@ -10,6 +10,7 @@ public class SpringConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://172.31.249.181:3000")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("*");
     }
 }
