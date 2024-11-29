8fcd2e4957fbb3b1643896d4c59a19c25427bf9f
Anir-sadiqui <anirsadiqui2@gmail.com>
2024-11-29 15:37:09 +0100

    up

--- src/main/java/Episante/back/SpringConfiguration.java
+++ src/main/java/Episante/back/SpringConfiguration.java
@@ -9,7 +9,7 @@ public class SpringConfiguration implements WebMvcConfigurer {
     @Override   
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://172.31.249.181:3000")
                 .allowedMethods("*");
     }
 }
